9fe317fa2274ee4db6e1bbc7c3c76ded7fc285ef
Noman-Prime <yaqoobn024@gmail.com>
2025-11-27 19:31:25 +0500

    added post

--- Server.js
+++ Server.js
@@ -17,5 +17,5 @@ app.use("/api/v1/project", routerProject);
 app.use("/api/v1/admin", routerAdmin);
 app.use("/api/v1/login",routerLogin)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT|| 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
